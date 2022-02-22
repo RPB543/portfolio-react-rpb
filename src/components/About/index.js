d7c20eeb741d3e0e0f2f89cb6a99cadce7f742f1
Becca Byrne <aggiebec06@gmail.com>
2022-02-22 17:48:25 -0600

    created initial components

---
+++ src/components/About/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function About() {
+    return (
+        <section className="my-5">
+            <h1 id="about">Who am I?</h1>
+            <p>Bob walks to work in the rain and snow.  </p>
+        </section>
+    );
+}
+
+export default About;
