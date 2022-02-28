db52eb2fd2ae2d165b5426c11158b992f04132f8
Becca Byrne <aggiebec06@gmail.com>
2022-02-27 22:24:35 -0600

    added picutres

--- src/components/Portfolio/index.js
+++ src/components/Portfolio/index.js
@@ -7,35 +7,35 @@ function Portfolio() {
             <h2>Projects</h2>
             <div class="grid-container">
 
-        <a href="https://obscure-hamlet-96065.herokuapp.com/" class="grid-item note-taker">
+        <a href="https://obscure-hamlet-96065.herokuapp.com/" target="blank" class="grid-item note-taker">
           <div>
             <h3>Note Taker</h3>
             <span>JavaScript/CSS/Express</span>
           </div>
         </a>
         
-        <a href="https://rpb543.github.io/awesome-weather-app/" class="grid-item weather-dashboard">
+        <a href="https://rpb543.github.io/awesome-weather-app/" target="blank" class="grid-item weather">
           <div>
             <h3>Weather Dashboard</h3>
             <span>JavaScript/HTML/CSS</span>
           </div>
         </a>
 
-        <a href="https://blooming-basin-79626.herokuapp.com/" class="grid-item budget-tracker">
+        <a href="https://blooming-basin-79626.herokuapp.com/" target="blank" class="grid-item budget-tracker">
           <div>
             <h3>Budget Tracker</h3>
             <span>NoSQL/Mongo/PWA</span>
           </div>
         </a>
         
-        <a href="https://mavisyupyup1.github.io/project-01-send-a-joke/" class="grid-item send-joke">
+        <a href="https://mavisyupyup1.github.io/project-01-send-a-joke/" target="blank" class="grid-item send-joke">
           <div>
             <h3>Send a Joke</h3>
             <span>HTML/CSS/JavaScript</span>
           </div>
         </a>
         
-        <a href="https://obscure-beach-84697.herokuapp.com/" class="grid-item tech-blog">
+        <a href="https://obscure-beach-84697.herokuapp.com/" target="blank" class="grid-item tech-blog">
           <div>
             <h3>Tech Blog</h3>
             <span>Handlebars/SQL/Express</span>
